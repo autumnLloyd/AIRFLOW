3c37bfc3630583f621762dc49dfd11ec368a4ae0
calvin <calvingoldenreal@gmail.com>
2025-10-14 22:14:03 -0400

    add auth providers. I'd do more but i have 2 hours of break left

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideHttpClient(),
   ]
 };
+// TODO Incorporate one of these later https://dev.to/this-is-angular/firebase-authentication-with-angular-19-ief 
