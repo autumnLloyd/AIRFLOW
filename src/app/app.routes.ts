b277f6e7fab0196cbbf4954e00f8030805be0de6
calvin <calvingoldenreal@gmail.com>
2025-10-18 18:48:28 -0400

    finish basic login setup. Bootstrapping does not work yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { ManagerLoginComponent } from './login/manager-login-component';
+import { TenantLoginComponent } from './login/tenant-login-component';
+import { ManagerDashboardComponent } from './dashboard/manager-dashboard-component';
+import { TenantDashboardComponent } from './dashboard/tenant-dashboard-component';
+import { managerGuard, tenantGuard } from './auth.guard';
 
-export const routes: Routes = [];
+// Creates guarded routes between pages; This is the "route" object all the components reference
+export const routes: Routes = [
+    {path: '', redirectTo: 'login/tenant', pathMatch: 'full'},
+
+    // public login pages
+    {path: 'login/manager', component: ManagerLoginComponent},
+    {path: 'login/tenant', component: TenantLoginComponent},
+
+    // private dashboards
+    {
+        path: 'manager/dashboard',
+        component: ManagerDashboardComponent,
+        canActivate: [managerGuard],
+    },
+    {
+        path: 'tenant/dashboard',
+        component: TenantDashboardComponent,
+        canActivate: [tenantGuard],
+    },
+
+    // catchall redirect
+    { path: "*", redirectTo: 'login/tenant'}
+];
