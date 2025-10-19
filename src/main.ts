4fdabbbd6a63ee28088944cdc1ba1c641376c8d4
calvin <calvingoldenreal@gmail.com>
2025-10-19 12:48:13 -0400

    fix bootstrapping and links between login pages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import 'zone.js';
 
 bootstrapApplication(AppComponent, {
   providers: [
