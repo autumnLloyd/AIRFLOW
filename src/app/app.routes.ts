6fac2d692586f13b8ba8e1a1b0f619791eca3db2
Autumn Rose Lloyd <lloydar@g.cofc.edu>
2025-10-20 01:39:40 -0400

    Implemented front-end styling, such as an intiial launch page. Did lots of restyling. TODO: Back button!!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ManagerLoginComponent } from './login/manager-login-component';
 import { TenantLoginComponent } from './login/tenant-login-component';
+import { LaunchLoginComponent } from './login/launch-login-component';
 import { ManagerDashboardComponent } from './dashboard/manager-dashboard.component';
 import { TenantDashboardComponent } from './dashboard/tenant-dashboard-component';
 import { managerGuard, tenantGuard } from './auth.guard';
@@ -8,11 +9,12 @@ import { PasskeyManagerComponent } from './manager/passkey.manager.component';
 
 // Creates guarded routes between pages; This is the "route" object all the components reference
 export const routes: Routes = [
-    {path: '', redirectTo: 'login/tenant', pathMatch: 'full'},
+    {path: '', redirectTo: 'login/launch', pathMatch: 'full'},
 
     // public login pages
     {path: 'login/manager', component: ManagerLoginComponent},
     {path: 'login/tenant', component: TenantLoginComponent},
+    {path: 'login/launch', component: LaunchLoginComponent},
 
     // private dashboards
     {
