f716625ad1b243d3048428ba27fde5fda93956ea
calvin <calvingoldenreal@gmail.com>
2025-10-19 21:07:14 -0400

    add manager dashboard and passkey create/update dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ManagerLoginComponent } from './login/manager-login-component';
 import { TenantLoginComponent } from './login/tenant-login-component';
-import { ManagerDashboardComponent } from './dashboard/manager-dashboard-component';
+import { ManagerDashboardComponent } from './dashboard/manager-dashboard.component';
 import { TenantDashboardComponent } from './dashboard/tenant-dashboard-component';
 import { managerGuard, tenantGuard } from './auth.guard';
+import { PasskeyManagerComponent } from './manager/passkey.manager.component';
 
 // Creates guarded routes between pages; This is the "route" object all the components reference
 export const routes: Routes = [
@@ -15,10 +16,14 @@ export const routes: Routes = [
 
     // private dashboards
     {
-        path: 'manager/dashboard',
-        component: ManagerDashboardComponent,
-        canActivate: [managerGuard],
-    },
+    path: 'manager',
+    canActivate: [managerGuard],
+    children: [
+      { path: 'dashboard', component: ManagerDashboardComponent },
+      { path: 'passkeys', component: PasskeyManagerComponent },
+      // (Future) { path: 'workorders', component: WorkOrderListComponent },
+    ]
+  },
     {
         path: 'tenant/dashboard',
         component: TenantDashboardComponent,
