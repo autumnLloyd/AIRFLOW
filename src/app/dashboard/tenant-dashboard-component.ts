666e928f91c12e63bf0b5e858ca64eb01d5f6892
Autumn Rose Lloyd <lloydar@g.cofc.edu>
2025-10-23 01:34:33 -0400

    Tenants can now file work orders.

--- src/app/dashboard/tenant-dashboard-component.ts
+++ src/app/dashboard/tenant-dashboard-component.ts
@@ -3,21 +3,35 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { ApplianceService } from '../services/appliance.service';
+import { WorkOrderService } from '../services/workorder.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tenant-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tenant-dashboard.component.html'
 })
 export class TenantDashboardComponent implements OnInit {
   appliances: any[] = [];
 
+  workorders: any[] = [];
+
+    newWorkOrder = {
+    applianceID: '',
+    created: '',
+    notes: '',
+    status:'',
+    updated:'',
+
+    }
+
   constructor(
     public auth: AuthService,
     private router: Router,
     private applianceService: ApplianceService,
-    private cdr: ChangeDetectorRef // <-- Inject here
+     private workOrderService: WorkOrderService,
+    private cdr: ChangeDetectorRef 
   ) {}
 
   async ngOnInit() {
@@ -35,6 +49,31 @@ export class TenantDashboardComponent implements OnInit {
 
     this.cdr.detectChanges(); // <-- Tell Angular to update the view
   }
+
+  async addWorkOrder() {
+    try {
+      const id = await this.workOrderService.createWorkOrder(this.newWorkOrder);
+      console.log('Appliance added with ID:', id);
+
+      // Refresh all workorders (manager view)
+      this.workorders = await this.workOrderService.getAllWorkOrders();
+      
+
+      // Clear form after adding
+      this.newWorkOrder = {
+        applianceID: '',
+        created: '',
+        notes: '',
+        status:'',
+        updated:'',
+      };
+
+      this.cdr.detectChanges(); // Trigger UI update
+    } catch (error) {
+      console.error('Failed to add appliance:', error);
+    }
+  }
+
 
   async logout() {
     await this.auth.logout();
